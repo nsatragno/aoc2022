fa27b641eceb677c11901aedb91b3ca1633bd5cf
Nina Satragno <nsatragno@chromium.org>
2022-12-15 19:42:45 -0400

    Day 15/Part 1 optimized so it runs

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,6 +1,10 @@
-use std::{fs, str::Chars};
+use std::{fs, str::Chars, collections::HashMap};
 
 type Coordinate = (i32, i32);
+
+const DEBUG: bool = false;
+const TARGET_Y: i32 = 2_000_000;
+//const TARGET_Y: i32 = 10;
 
 struct Sensor {
     position: Coordinate,
@@ -43,86 +47,82 @@ fn distance(sensor: &Sensor) -> i32 {
 fn main() {
     let file = fs::read_to_string("input.txt").unwrap();
     let sensors: Vec<Sensor> = file.trim().split('\n').map(Sensor::from).collect();
+    let mut map: HashMap<Coordinate, char> = HashMap::new();
 
-    let mut max_x = 0;
-    let mut max_y = 0;
 
-    let mut min_x: i32 = 0;
-    let mut min_y: i32 = 0;
+    let mut i = 0;
     for sensor in &sensors {
-        let distance = distance(&sensor);
-        let right_shift = sensor.position.0 + distance;
-        if right_shift > max_x {
-            max_x = right_shift
-        }
-        let bottom_shift = sensor.position.1 + distance;
-        if bottom_shift > max_y {
-            max_y = bottom_shift;
-        }
-        let left_shift = sensor.position.0 - distance;
-        if left_shift < min_x {
-            min_x = left_shift;
-        }
-        let top_shift = sensor.position.1 - distance;
-        if top_shift < min_y {
-            min_y = top_shift;
-        }
-    }
+        i += 1;
+        println!("Sensor {} / {}", i, sensors.len());
+        map.insert(sensor.position, 'S');
+        map.insert(sensor.beacon, 'B');
 
-    let shift_x = min_x.abs() as i32;
-    let shift_y = min_y.abs() as i32;
-    let mut map = vec![vec!['.'; (max_y + 1 + shift_y) as usize]; (max_x + 1 + shift_x) as usize];
-    println!("Shift x: {}", shift_x);
-    println!("Shift y: {}", shift_y);
-    println!("Map x: {}", map.len());
-    println!("Map y: {}", map[0].len());
-
-    for sensor in &sensors {
-        let x = (sensor.position.0 + shift_x) as usize;
-        let y = (sensor.position.1 + shift_y) as usize;
-        map[x][y] = 'S';
-
-        let x = (sensor.beacon.0 + shift_x) as usize;
-        let y = (sensor.beacon.1 + shift_y) as usize;
-        map[x][y] = 'B';
-
-        let distance = (sensor.position.0.abs_diff(sensor.beacon.0)
-            + sensor.position.1.abs_diff(sensor.beacon.1)) as i32;
-        for x in sensor.position.0 - distance..=sensor.position.0 + distance {
+        let distance = distance(sensor);
+        /*for x in sensor.position.0 - distance..=sensor.position.0 + distance {
             let distance = distance - (x.abs_diff(sensor.position.0)) as i32;
             for y in sensor.position.1 - distance..=sensor.position.1 + distance {
-                let x = (x + shift_x) as usize;
-                let y = (y + shift_y) as usize;
-                if map[x][y] == '.' {
-                    map[x][y] = '#';
+                if y == TARGET_Y {
+                    if map.get(&(x, y)).is_none() {
+                        map.insert((x, y), '#');
+                    }
+                }
+            }
+        }*/
+        for y in sensor.position.1 - distance..=sensor.position.1 + distance {
+            if y != TARGET_Y {
+                continue;
+            }
+            let distance = distance - (y.abs_diff(sensor.position.1)) as i32;
+            for x in sensor.position.0 - distance..=sensor.position.0 + distance {
+                if map.get(&(x, y)).is_none() {
+                    map.insert((x, y), '#');
                 }
             }
         }
     }
 
+    let mut min_x = i32::MAX;
+    let mut min_y = i32::MAX;
+    let mut max_x = 0;
+    let mut max_y = 0;
+    for point in map.keys() {
+        min_x = min_x.min(point.0);
+        min_y = min_y.min(point.1);
+        max_x = max_x.max(point.0);
+        max_y = max_y.max(point.1);
+    }
     let mut count = 0;
-    for x in 0..map.len() {
-        let point = map[x][(10 + shift_y) as usize];
-        if point == '#' || point == 'S' {
-            count += 1;
+    for x in min_x..=max_x {
+        let point = map.get(&(x, TARGET_Y));
+        if let Some(point) = point {
+            if *point == '#' || *point == 'S' {
+                count += 1;
+            }
         }
     }
 
-    print!("  ");
-    for x in 0..map.len() {
-        if (x as i32 - shift_x as i32) % 5 == 0 {
-            print!("{:2} ", x as i32 - shift_x as i32);
-        } else {
-            print!(" ");
-        }
-    }
-    print!("\n");
-    for y in 0..map[0].len() {
-        print!("{:2} ", y as i32 - shift_y as i32);
-        for x in 0..map.len() {
-            print!("{}", map[x][y]);
+    if DEBUG {
+        print!("  ");
+        for x in min_x..=max_x {
+            if x % 5 == 0 {
+                print!("{:2} ", x);
+            } else {
+                print!(" ");
+            }
         }
         print!("\n");
+        for y in min_y..=max_y {
+            print!("{:2} ", y);
+            for x in min_x..=max_x {
+                if let Some(value) = map.get(&(x, y)) {
+                    print!("{}", value);
+                } else {
+                    print!(".");
+                }
+            }
+            print!("\n");
+        }
     }
+
     println!("The count is {count}");
 }
