3e7840c285d3772997558d81d9d8be6abbcb1f78
Nina Satragno <nsatragno@chromium.org>
2022-12-25 14:20:09 -0400

    le cube.

--- day22_part2/src/main.rs
+++ day22_part2/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::{HashMap},
-    fs,
+    fs, ops::Bound,
 };
 
 use euclid::{default::Point3D, Angle, Rotation3D, UnknownUnit, Vector3D };
@@ -9,43 +9,67 @@ type Coordinate = (i64, i64);
 
 const DIRECTIONS: &'static [(i64, i64)] = &[(-1, 0), (1, 0), (0, 1), (0, -1)];
 
-/* 
 struct BoundingBox {
-    min_x: i64,
-    max_x: i64,
-    min_y: i64,
-    max_y: i64,
-}
-
-impl BoundingBox {
-    fn from(map: &HashMap<Coordinate, bool>) -> BoundingBox {
-        let mut bounding_box = BoundingBox {
-            min_x: i64::MAX,
-            max_x: 0,
-            min_y: i64::MAX,
-            max_y: 0,
-        };
-        for coordinate in map.keys() {
-            bounding_box.min_x = bounding_box.min_x.min(coordinate.0);
-            bounding_box.max_x = bounding_box.max_x.max(coordinate.0);
-            bounding_box.min_y = bounding_box.min_y.min(coordinate.1);
-            bounding_box.max_y = bounding_box.max_y.max(coordinate.1);
-        }
-        bounding_box
+       min_x: i64,
+       max_x: i64,
+       min_y: i64,
+       max_y: i64,
+   }
+   
+   impl BoundingBox {
+       fn from(map: &HashMap<Coordinate, bool>) -> BoundingBox {
+           let mut bounding_box = BoundingBox {
+               min_x: i64::MAX,
+               max_x: 0,
+               min_y: i64::MAX,
+               max_y: 0,
+           };
+           for coordinate in map.keys() {
+               bounding_box.min_x = bounding_box.min_x.min(coordinate.0);
+               bounding_box.max_x = bounding_box.max_x.max(coordinate.0);
+               bounding_box.min_y = bounding_box.min_y.min(coordinate.1);
+               bounding_box.max_y = bounding_box.max_y.max(coordinate.1);
+           }
+           bounding_box
+       }
     }
-}*/
 
-fn write_map(map: &HashMap<Coordinate, Point3D<i64>>) {
+fn write_map(map: &HashMap<Coordinate, Point3D<i64>>, width: i64) {
     unsafe {
         static mut NUM: usize = 0;
         let mut s = String::new();
-        for point in map.values() {
-            s += &format!("{}, {}, {}\n", point.x, point.y, point.z);
+
+        let colours = colour_map(map, width);
+        for colour in colours.values() {
+            for point in colour {
+                s += &format!("{}, {}, {}\n", point.x, point.y, point.z);
+            }
+            s += &format!("\n\n");
         }
         fs::write(format!("points{}.txt", NUM), s).unwrap();
         NUM += 1;
     }
 }
+
+
+fn colour_map(map: &HashMap<Coordinate, Point3D<i64>>, width: i64) -> HashMap<i64, Vec<Point3D<i64>>> {
+    let mut colours: HashMap<i64, Vec<Point3D<i64>>> = HashMap::new();
+    let bounding_box = BoundingBox::from(&map.keys().map(|k| (k.clone(), false)).collect());
+    for x in 0..=bounding_box.max_x {
+        for y in 0..=bounding_box.max_y {
+            if let Some(point) = map.get(&(x, y)) {
+                let coord = x / width as i64 + (y / width as i64) * width as i64;
+                if let Some(vec) = colours.get_mut(&coord) {
+                    vec.push(point.clone());
+                } else {
+                    colours.insert(coord, vec![point.clone()]);
+                }
+            }
+        }
+    }
+    colours
+}
+
 
 fn fold_cube(
     flat_map: &HashMap<Coordinate, bool>,
@@ -82,53 +106,59 @@ fn fold_cube_from(
             continue;
         }
         let attached = find_attached(&flat_map, &new_destination, &destination, width);
-        let (axis, translation) = match direction {
+        let axis = match direction {
             // Left:
-            (-1, 0) => ([
+            (-1, 0) => [
                 (destination.0, destination.1),
                 (destination.0, destination.1 + width - 1),
-            ], (destination.0, destination.1 + 1)),
+            ],
 
             // Right:
-            (1, 0) => ([
+            (1, 0) => [
                 (destination.0 + width - 1, destination.1 + width - 1),
                 (destination.0 + width - 1, destination.1),
-            ], (destination.0 + width - 1, destination.1)),
+            ],
 
             // Up:
-            (0, -1) => ([
+            (0, -1) => [
                 (destination.0, destination.1),
                 (destination.0 + width - 1, destination.1),
-            ], (destination.0, destination.1)),
+            ],
 
             // Down:
-            (0, 1) => ([
+            (0, 1) => [
                 (destination.0, destination.1 + width - 1),
                 (destination.0 + width - 1, destination.1 + width - 1),
-            ], (destination.0 + width, destination.1 + width - 1)),
+            ],
             _ => unreachable!(),
         };
         println!("Folding {:?}", direction);
         println!("Folding {} tiles along {:?}", attached.len(), axis);
 
+        let rotation_translation = map[&axis[0]].to_vector();
+        let close_distance_transation = map[&destination] - map[&new_destination];
+        let close_distance_transation: Vector3D<f64, UnknownUnit> = Vector3D::from((close_distance_transation.x as f64, close_distance_transation.y as f64, close_distance_transation.z as f64));
+        let close_distance_transation = close_distance_transation.normalize();
+        let close_distance_transation: Vector3D<i64, UnknownUnit> = Vector3D::from((close_distance_transation.x.round() as i64, close_distance_transation.y.round() as i64, close_distance_transation.z.round() as i64));
+
         let axis = map[&axis[1]] - map[&axis[0]];
         let axis = Vector3D::from((axis.x as f64, axis.y as f64, axis.z as f64));
 
-        let translation = map[&translation].to_vector();
         let rotation: Rotation3D<f64, UnknownUnit, UnknownUnit> =
             Rotation3D::around_axis(axis, Angle::degrees(90f64));
-        println!("Translation vector : {:?}", translation);
+        println!("Translation vector : {:?}", rotation_translation);
         println!("Rotation axis: {:?}", axis);
         for tile in attached {
             let point = map[&tile];
-            let point = point - translation;
+            let point = point - rotation_translation;
             let point = Point3D::from((point.x as f64, point.y as f64, point.z as f64));
             let point = rotation.transform_point3d(point);
             let point = Point3D::from((point.x.round() as i64, point.y.round() as i64, point.z.round() as i64));
-            let point = point + translation;
+            let point = point + rotation_translation;
+            let point = point - close_distance_transation;
             map.insert(tile.clone(), point);
         }
-        write_map(map);
+        write_map(map, width);
         fold_cube_from(&new_destination, destination, flat_map, map, width);
     }
 }
@@ -200,6 +230,6 @@ fn main() {
         })
         .collect();
 
-    write_map(&map);
+    write_map(&map, width);
     fold_cube(&flat_map, &mut map, width);
 }
